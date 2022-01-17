7814d646199603b61d5754f6b63ae9f175fd1253
JD Bartee <jd@bartee.net>
2022-01-17 09:01:17 -0600

    Initial Commit

---
+++ WordleClone/WordleCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleCloneApp.swift
+//  WordleClone
+//
+//  Created by JD Bartee on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
